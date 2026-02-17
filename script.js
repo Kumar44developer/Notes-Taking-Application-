0721648a8635c45f5c5681867a14599fae61e77e
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-02-17 18:41:25 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const addBtn = document.querySelector('#addBtn');
 const main = document.querySelector('#main');
 
+
 addBtn.addEventListener("click",addNote);
 
 function addNote(){
+
     const note = document.createElement("div");
     note.classList.add("note");
-        note.innerHTML = `
+    note.innerHTML = `
         <div class="tool">
             <i class="save fas fa-save"></i>
             <i class="trash fas fa-trash"></i>
@@ -14,58 +16,58 @@ function addNote(){
         <textarea></textarea>
     `;
 
+    const trashIcon = note.querySelector(".trash");
+    const saveIcon = note.querySelector(".save");
+    const textarea = note.querySelector("textarea");
 
-const trashIcon = note.querySelector(".trash");
-const saveIcon = note.querySelector(".save");
-const textarea = note.querySelector("textarea");
-
-trashIcon.addEventListener("click",()=>{
-    note.remove();
-    saveNote();
+    trashIcon.addEventListener("click",()=>{
+        note.remove();
+        saveNote();
 
     });
 
+    saveIcon.addEventListener("click",saveNote);
+    textarea.addEventListener("input",saveNote);
 
-saveIcon.addEventListener("click",saveNote);
-textarea.addEventListener("input",saveNote);
     main.appendChild(note);
 }
 
 
 function saveNote(){
+
     const notes = document.querySelectorAll(".note textarea")
-        const data = [];
+    const data = [];
+
     for(let i = 0; i < notes.length; i++)
     {
         data.push(notes[i].value);
     }
 
-if(data.length === 0){
-localStorage.removeItem("notes")
+    if(data.length === 0){
+        localStorage.removeItem("notes")
     }else{
-    localStorage.setItem("notes",JSON.stringify(data));
+        localStorage.setItem("notes",JSON.stringify(data));
     }
 
-}Close saveNote function or block
-
+}
 
 function loadNotes(){
     const lsNotes = JSON.parse(localStorage.getItem("notes"));
+
     if(lsNotes !== null){
         lsNotes.forEach(noteText =>{
-        addNote();
+            addNote();
 
-    const notes = document.querySelectorAll(".note textarea");
-    const lastNote = notes[notes.length - 1];
-    lastNote.value = noteText;
-});
-    
-}else{
+            const notes = document.querySelectorAll(".note textarea");
+            const lastNote = notes[notes.length - 1];
+            lastNote.value = noteText;
+
+        });
+
+    }else{
         addNote();
-    }   
+    }
 
 }
 
 loadNotes();
-
-    
